45400b0c75b8f87176906d8575e9d5abeb2a0eea
Tomancok, Alexander <alexander.tomancok@gmail.com>
2022-10-08 15:29:33 +0200

    add pattern unlock

--- lib/safe/unlock_pattern.dart
+++ lib/safe/unlock_pattern.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:pattern_lock/pattern_lock.dart';
+import 'package:senacor_devcon_mysafe/safe/unlock_biometric.dart';
 
 class UnlockPattern extends StatelessWidget {
   const UnlockPattern({Key? key}) : super(key: key);
@@ -9,9 +12,54 @@ class UnlockPattern extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Step 2'),
       ),
-      body: const Padding(
-        padding: EdgeInsets.all(16),
-        child: Text('Hello'),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Flexible(
+              child: Text('Draw the pattern'),
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            Flexible(
+              child: PatternLock(
+                fillPoints: true,
+                onInputComplete: (List<int> input) =>
+                    listEquals(input, [0, 3, 7, 5, 2])
+                        ? _showSuccess(context)
+                        : _showError(context),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _showSuccess(BuildContext context) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: const Text('Correct pattern!\nPlease move on.'),
+        duration: const Duration(days: 365),
+        action: SnackBarAction(
+          label: 'Continue',
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (_) => const UnlockBiometric()),
+            );
+          },
+        ),
+      ),
+    );
+  }
+
+  void _showError(BuildContext context) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      const SnackBar(
+        content: Text('Wrong pattern. Please try again.'),
       ),
     );
   }
